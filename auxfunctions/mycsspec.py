7d9ee1dbe1f69582e3b71b277d6875622eec1f14
skerzot <skerzot@gmail.com>
2020-06-12 17:48:03 -0500

    Cleaning imports

--- auxfunctions/mycsspec.py
+++ auxfunctions/mycsspec.py
@@ -24,15 +24,6 @@ import ctools
 from cscripts import mputils
 import numpy as np
 
-CURSOR_UP_ONE = '\x1b[1A'
-ERASE_LINE = '\x1b[2K'
-
-def delete_last_lines(n=1):
-    for _ in range(n):
-        sys.stdout.write( CURSOR_UP_ONE )
-        sys.stdout.write( ERASE_LINE )
-
-
 # ============ #
 # csspec class #
 # ============ #
